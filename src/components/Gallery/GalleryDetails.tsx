32238c7323cf0b47b1de372f92d0d83026682e6e
SiddikArim <arim99999@gmail.com>
2024-02-07 18:19:20 +0600

    gallery complete

--- src/components/Gallery/GalleryDetails.tsx
+++ src/components/Gallery/GalleryDetails.tsx
@@ -1,13 +1,13 @@
 const GalleryDetails = () => {
   return (
-    <div className="w-1/2">
-      <h1>Gallery</h1>
+    <div className="w-1/2 my-auto flex flex-col px-10">
+      <h1 className="text-5xl font-bold mb-4 py-2">Gallery Details</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi distinctio
         beatae error, quod eius obcaecati corporis consequuntur illo quaerat
         suscipit.
       </p>
-      <ul>
+      <ul className="font-semibold py-5">
         <li>something here</li>
         <li>something here</li>
         <li>something here</li>
--- src/components/Gallery/ImgGallery.tsx
+++ src/components/Gallery/ImgGallery.tsx
@@ -1,7 +1,39 @@
+import birthday from "../../assets/birthday.jpg";
+import corporation from "../../assets/corporation.jpg";
+import family from "../../assets/family.jpg";
+import picnic from "../../assets/picnic.jpg";
+import sports from "../../assets/sports.jpg";
+import wed from "../../assets/wed.jpg";
+import wed2 from "../../assets/wed2.jpg";
+import anime from "../../assets/anime.jpg";
+import anime2 from "../../assets/anime2.jpg";
+import gather from "../../assets/gather.jpg";
+
 const ImgGallery = () => {
   return (
-    <div className="w-1/2">
-      <h1>Img will be here</h1>
+    <div className="w-[636px] p-2 h-[651] grid grid-cols-12">
+      <div className="col-span-3 my-auto ">
+        <img
+          className="py-1 ml-6 w-4/5 rounded-2xl"
+          src={gather}
+          alt="gathering"
+        />
+        <img className="pr-2 rounded-2xl h-52 w-50" src={anime2} alt="" />
+      </div>
+      <div className="col-span-3 max-w-[628px] gap-1">
+        <img className="" src={wed} alt="" />
+        <img className="py-1" src={wed2} alt="" />
+        <img className="py-1" src={picnic} alt="" />
+      </div>
+      <div className="col-span-3 py-8">
+        <img className="p-2 rounded-2xl" src={anime} alt="" />
+        <img className="p-2 rounded-2xl" src={sports} alt="" />
+        <img className="p-2 rounded-2xl" src={corporation} alt="" />
+      </div>
+      <div className="col-span-3 py-24">
+        <img className="py-1" src={family} alt="" />
+        <img className="py-1" src={birthday} alt="" />
+      </div>
     </div>
   );
 };
